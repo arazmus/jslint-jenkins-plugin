eea5fa582d9db082e27ef6442931ad329e519f0f
Scott Jones <scott.jones@rivieratravel.co.uk>
2013-01-17 17:08:21 +0000

    added a replace to remove %20 from windows locations

--- src/main/java/com/boxuk/jenkins/jslint/LintRunner.java
+++ src/main/java/com/boxuk/jenkins/jslint/LintRunner.java
@@ -102,7 +102,7 @@ public class LintRunner implements Callable<Properties, RuntimeException> {
             if(res == null) {
                 this.getClass().getResource("JSLint/jslint.js");
             }
-            String file = res.toString().replaceAll("file:", "");
+            String file = res.toString().replaceAll("file:", "").replaceAll("%20", " ");
 
             try {
                 context.evaluateReader(
